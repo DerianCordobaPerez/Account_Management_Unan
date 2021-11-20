c851f06cba3ee827d453953abe468729a510a7f1
santelizvargas <ushija6@gmail.com>
2021-11-20 15:12:12 -0600

    Creacion de los componentes navbar y footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,1 +1,57 @@
 <!-- TODO: Footer -->
+<footer class="bg-light text-white footer_style">
+    <!-- Grid container -->
+    <div class="container p-3 pb-0 d-flex justify-content-betweens row mx-4">
+        <div class="col align-items-start justify-content-start text-dark">
+            <h4 class=" text_black">Mantente en contacto</h4>
+            <p>UNAN - Leon Nicaragua<br>
+                <a href="http://unanleon.edu.ni" target="blank">
+                    <i class="bi bi-globe text-dark"></i> http://unanleon.edu.ni
+                </a> <br>
+                <a href="#">
+                    <i class="bi bi-telephone text-dark"></i> PBX: +505-23115013 | FAX: +505-23114970 
+                </a><br>
+                <a href="mailto:avconsultas@ac.unanleon.edu.ni">
+                    <i class="bi bi-envelope text-dark"></i> avconsultas@ac.unanleon.edu.ni
+                </a> </p>
+        </div>
+        <!-- Section: Social media -->
+        <div class="col d-flex align-items-end justify-content-end">
+            <section class="mb-4">
+                <h5 class="text_black text-center">Redes Sociales</h5>
+                <a class="btn btn-primary networks btn-floating m-1" style="background-color: #3b5998;"
+                    href="https://www.facebook.com/BicentenariaUNANLeon/" role="button" target="blank"><i
+                        class="fab fa-facebook-f"></i></a>
+
+                <!-- Twitter -->
+                <a class="btn btn-primary networks btn-floating m-1" style="background-color: #55acee;"
+                    href="https://twitter.com/UNANLeon" role="button" target="blank"><i class="fab fa-twitter"></i></a>
+
+                <!-- Instagram -->
+                <a class="btn networks btn-primary btn-floating m-1" style="background-color: #ac2bac;"
+                    href="https://www.instagram.com/unan_leon/" role="button" target="blank"><i
+                        class="fab fa-instagram"></i></a>
+
+                <!-- Linkedin -->
+                <a class="btn btn-primary networks btn-floating m-1" style="background-color: #0082ca;"
+                    href=" https://www.linkedin.com/company/bicentenariaunanleon/" role="button" target="blank"><i
+                        class="fab fa-linkedin-in"></i></a>
+                <!-- YouTube -->
+                <a class="btn btn-primary networks btn-floating m-1" style="background-color: #cc1111;"
+                    href="https://www.youtube.com/channel/UC-Jhww9XPbavBsMDtG_Rv4A" role="button" target="blank"><i
+                        class="fab fa-youtube"></i></a>
+            </section>
+        </div>
+        <!-- Section: Social media -->
+    </div>
+    <!-- Grid container -->
+    <div class="container">
+        <hr>
+    </div>
+
+    <!-- Copyright -->
+    <div class="text-center bg-light text_black">
+        <h6>A la libertad por la universidad</h6>
+    </div>
+    <!-- Copyright -->
+</footer>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,63 @@
 @extends('layouts.app')
 
 @section('content')
-    <h2 class="text-muted">Hola mundo</h2>
+<div class="row">
+    <div class="col">
+        <div class="card mb-3" style="max-width: 540px;">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img src="https://avirtual.unanleon.edu.ni/img/UNANLEON_principal.png" class="img-fluid rounded-start" alt="...">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">UNAN - Leon</h5>
+              <p class="card-text">Una vez concluido el proceso de inscripción, imprima su hoja de inscripción o tome una captura de pantalla con los datos generados.</p>
+              <p class="card-text"><small class="text-muted">Matriculas en linea</small></p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="card" style="max-width: 540px;">
+        <div class="card-body">
+          <div class="form-check">
+            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+            <label class="form-check-label" for="flexRadioDefault1">
+              Default radio
+            </label>
+          </div>
+          <div class="form-check">
+            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked>
+            <label class="form-check-label" for="flexRadioDefault2">
+              Default radio
+            </label>
+          </div>
+          <div class="form-check">
+            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+            <label class="form-check-label" for="flexRadioDefault1">
+              Default radio
+            </label>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+        <form>
+            <div class="mb-3">
+              <label for="exampleInputEmail1" class="form-label">Email address</label>
+              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+              <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+            </div>
+            <div class="mb-3">
+              <label for="exampleInputPassword1" class="form-label">Password</label>
+              <input type="password" class="form-control" id="exampleInputPassword1">
+            </div>
+            <div class="mb-3 form-check">
+              <input type="checkbox" class="form-check-input" id="exampleCheck1">
+              <label class="form-check-label" for="exampleCheck1">Check me out</label>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </form>
+    </div>
+</div>
+      
 @endsection
