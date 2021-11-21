1feb45ab390649551a9eab826a27226c7e6e28ca
DerianCordobaPerez <derianunanleon@gmail.com>
2021-11-21 00:04:47 -0600

    Creating the page about and modifying the navbar component

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,73 +1,66 @@
 <!-- TODO: Navbar -->
-<!DOCTYPE html>
-<html lang="en">
 
-<body>
-    <!-- navigation bar start -->
-    <header>
-        <nav class="navbar navbar-expand-lg d-flex navegation_bar_style">
-            <div class="container-fluid d-flex justify-content-between mx-4">
-                <div class="mx-4">
-                    <h3 class="navbar-brand title-bar">Account Management <i class="bi bi-github"></i></h3>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                        aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                </div>
-                <div class="mx-4 collapse navbar-collapse d-flex align-items-end flex-column" id="navbarSupportedContent">
-                    <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
-                        <li class="nav-item me-2 option_nav_bar ">
-                            <a class="nav-link text-dark" aria-current="true"href="#"><i class="bi bi-house"></i> Inicio</a>
-                        </li>
-
-                        <!-- Option 1 dropdown -->
-                        <li class="nav-item dropdown option_nav_bar">
-                            <a class="nav-link text-dark" href="#" id="navbarDropdown" role="button"
-                                data-bs-toggle="dropdown" aria-expanded="false">
-                                <i class="bi bi-list-check"></i> Opciones 
-                        </a>
-                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><a class="dropdown-item" href="#">option 1</a></li>
-                                <li><a class="dropdown-item" href="#">option 2</a></li>
-                                <li><a class="dropdown-item" href="#">option 3</a></li>
-                                <li><a class="dropdown-item" href="#">option 4</a></li>
-                            </ul>
-                        </li>
-
-                        <!-- Option 2 dropdown -->
-                        <li class="nav-item dropdown option_nav_bar">
-                            <a class="nav-link text-dark" href="#" id="navbarDropdown" role="button"
-                                data-bs-toggle="dropdown" aria-expanded="false">
-                                <i class="bi bi-list-check"></i> Opciones 
-                            </a>
-                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><a class="dropdown-item" href="#">option 1</a></li>
-                                <li><a class="dropdown-item" href="#">option 2</a></li>
-                                <li><a class="dropdown-item" href="#">option 3</a></li>
-                                <li><a class="dropdown-item" href="#">option 4</a></li>
-                            </ul>
-                        </li>
-
-                        <!-- Option 3 dropdown -->
-                        <li class="nav-item dropdown option_nav_bar">
-                            <a class="nav-link text-dark" href="#" id="navbarDropdown" role="button"
-                                data-bs-toggle="dropdown" aria-expanded="false">
-                                <i class="bi bi-list-check"></i> Opciones 
-                            </a>
-                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><a class="dropdown-item" href="#">option 1</a></li>
-                                <li><a class="dropdown-item" href="#">option 2</a></li>
-                                <li><a class="dropdown-item" href="#">option 3</a></li>
-                                <li><a class="dropdown-item" href="#">option 4</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                </div>
+<!-- navigation bar start -->
+<header>
+    <nav class="navbar navbar-expand-lg d-flex navegation_bar_style">
+        <div class="container-fluid d-flex justify-content-between mx-4">
+            <div class="mx-4">
+                <h3 class="navbar-brand title-bar">Unan Leon<i class="bi bi-github"></i></h3>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
             </div>
-        </nav>
-    </header>
+            <div class="mx-4 collapse navbar-collapse d-flex align-items-end flex-column" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
+                    <li class="nav-item me-2 option_nav_bar ">
+                        <a class="nav-link text-dark" aria-current="true"href="#"><i class="bi bi-house"></i> Inicio</a>
+                    </li>
 
-</body>
+                    <!-- Option 1 dropdown -->
+                    <li class="nav-item dropdown option_nav_bar">
+                        <a class="nav-link text-dark" href="#" id="navbarDropdown" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            <i class="bi bi-list-check"></i> Opciones
+                    </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="#">option 1</a></li>
+                            <li><a class="dropdown-item" href="#">option 2</a></li>
+                            <li><a class="dropdown-item" href="#">option 3</a></li>
+                            <li><a class="dropdown-item" href="#">option 4</a></li>
+                        </ul>
+                    </li>
 
-</html>
+                    <!-- Option 2 dropdown -->
+                    <li class="nav-item dropdown option_nav_bar">
+                        <a class="nav-link text-dark" href="#" id="navbarDropdown" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            <i class="bi bi-list-check"></i> Opciones
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="#">option 1</a></li>
+                            <li><a class="dropdown-item" href="#">option 2</a></li>
+                            <li><a class="dropdown-item" href="#">option 3</a></li>
+                            <li><a class="dropdown-item" href="#">option 4</a></li>
+                        </ul>
+                    </li>
+
+                    <!-- Option 3 dropdown -->
+                    <li class="nav-item dropdown option_nav_bar">
+                        <a class="nav-link text-dark" href="#" id="navbarDropdown" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            <i class="bi bi-list-check"></i> Opciones
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="#">option 1</a></li>
+                            <li><a class="dropdown-item" href="#">option 2</a></li>
+                            <li><a class="dropdown-item" href="#">option 3</a></li>
+                            <li><a class="dropdown-item" href="#">option 4</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
