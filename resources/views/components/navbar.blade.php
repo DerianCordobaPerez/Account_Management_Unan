3ce8330aa98725d03b8f27fc79562bfdcbd7990a
santelizvargas <ushija6@gmail.com>
2021-11-22 01:50:24 -0600

    login edit

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -12,66 +12,79 @@
 
         <div class="collapse navbar-collapse " id="navbarSupportedContent">
             <ul class="navbar-nav ml-auto ms-auto mb-2 mb-lg-0">
-                <li class="nav-item me-2">
-                    <a class="nav-link text-dark" aria-current="true" href="{{route('home')}}">
-                        <i class="bi bi-house"></i>
-                        Inicio
-                    </a>
-                </li>
+               
+                @guest
+                    <li class="nav-item me-2">
+                        <a class="nav-link text-dark" aria-current="true" href="{{route('login')}}">
+                            <i class="bi bi-door-open"></i>
+                            Iniciar Sesion
+                        </a>
+                    </li>
+                @endguest
 
-                <!-- Option 1 dropdown -->
-                <li class="nav-item dropdown">
-                    <a class="nav-link text-dark" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        <i class="bi bi-list-check"></i>
-                        Opciones
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="#">option 1</a></li>
-                        <li><a class="dropdown-item" href="#">option 2</a></li>
-                        <li><a class="dropdown-item" href="#">option 3</a></li>
-                        <li><a class="dropdown-item" href="#">option 4</a></li>
-                    </ul>
-                </li>
+                @auth
+                    <li class="nav-item me-2">
+                        <a class="nav-link text-dark" aria-current="true" href="{{route('home')}}">
+                            <i class="bi bi-house"></i>
+                            Inicio
+                        </a>
+                    </li>
 
-                <!-- Option 2 dropdown -->
-                <li class="nav-item dropdown">
-                    <a class="nav-link text-dark" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        <i class="bi bi-list-check"></i>
-                        Opciones
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="#">option 1</a></li>
-                        <li><a class="dropdown-item" href="#">option 2</a></li>
-                        <li><a class="dropdown-item" href="#">option 3</a></li>
-                        <li><a class="dropdown-item" href="#">option 4</a></li>
-                    </ul>
-                </li>
+                        <!-- Option 1 dropdown -->
+                    <li class="nav-item dropdown">
+                        <a class="nav-link text-dark" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <i class="bi bi-list-check"></i>
+                            Opciones
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="#">option 1</a></li>
+                            <li><a class="dropdown-item" href="#">option 2</a></li>
+                            <li><a class="dropdown-item" href="#">option 3</a></li>
+                            <li><a class="dropdown-item" href="#">option 4</a></li>
+                        </ul>
+                    </li>
 
-                <!-- Option 3 dropdown -->
-                <li class="nav-item dropdown">
-                    <a class="nav-link text-dark" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        <i class="bi bi-list-check"></i>
-                        Opciones
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="#">option 1</a></li>
-                        <li><a class="dropdown-item" href="#">option 2</a></li>
-                        <li><a class="dropdown-item" href="#">option 3</a></li>
-                        <li><a class="dropdown-item" href="#">option 4</a></li>
-                    </ul>
-                </li>
+                    <!-- Option 2 dropdown -->
+                    <li class="nav-item dropdown">
+                        <a class="nav-link text-dark" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <i class="bi bi-list-check"></i>
+                            Opciones
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="#">option 1</a></li>
+                            <li><a class="dropdown-item" href="#">option 2</a></li>
+                            <li><a class="dropdown-item" href="#">option 3</a></li>
+                            <li><a class="dropdown-item" href="#">option 4</a></li>
+                        </ul>
+                    </li>
 
-                <li class="nav-item me-2">
-                    <a class="nav-link text-dark" href="{{ route('logout') }}"
-                       onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                        <i class="bi bi-power"></i>
-                        Cerrar sesión
-                    </a>
+                    <!-- Option 3 dropdown -->
+                    <li class="nav-item dropdown">
+                        <a class="nav-link text-dark" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <i class="bi bi-list-check"></i>
+                            Opciones
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="#">option 1</a></li>
+                            <li><a class="dropdown-item" href="#">option 2</a></li>
+                            <li><a class="dropdown-item" href="#">option 3</a></li>
+                            <li><a class="dropdown-item" href="#">option 4</a></li>
+                        </ul>
+                    </li>
 
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                        @csrf
-                    </form>
-                </li>
+                    <li class="nav-item me-2">
+                        <a class="nav-link text-dark" href="{{ route('logout') }}"
+                        onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                            <i class="bi bi-power"></i>
+                            Cerrar sesión
+                        </a>
+
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
+                    </li>
+                @endauth
+               
             </ul>
 
         </div>
