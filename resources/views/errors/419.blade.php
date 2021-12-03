651a97fa2dc3d7683cb785a194a110218b38fe75
DerianCordobaPerez <derianunanleon@gmail.com>
2021-12-03 03:22:04 -0600

    Change within the implementation system about session downtime

---
+++ resources/views/errors/419.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="row justify-content-center mt-4">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('419 - Pagina expirada') }}</div>
+
+                <div class="card-body">
+                    <p>Su sesión ha expirado, por favor ingrese nuevamente.</p>
+                    <a class="btn btn-primary" href="{{ route('login') }}">{{ __('Iniciar sesion') }}</a>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
