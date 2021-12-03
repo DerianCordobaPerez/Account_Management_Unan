61e73262f134c438aa1a1406804e26fead5ab268
santelizvargas <ushija6@gmail.com>
2021-12-03 05:08:25 -0600

    user view modifications

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,9 +1,9 @@
 <!-- navbar start -->
 <nav class="navbar navbar-expand-sm p-0 m-0">
-    <div class="container-fluid px-4 w">
+    <div class="container-fluid px-4">
         <h3 class="navbar-brand title-bar">
-            Unan Leon
             <img src="{{ asset('img/Logo.png')}}" alt="" width="25">
+            Unan Leon
         </h3>
 
         <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">
@@ -13,16 +13,6 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ml-auto ms-auto mb-2 mb-lg-0">
                
-                @guest
-                    <li class="nav-item me-2">
-                        <a class="nav-link text-dark" aria-current="true" href="{{route('login')}}">
-                            <i class="bi bi-door-open"></i>
-                            Iniciar Sesion
-                        </a>
-                    </li>
-                @endguest
-
-                @auth
                     <li class="nav-item navbar-option" style="height: 100%">
                         <a class="nav-link" aria-current="true" href="{{route('home')}}">
                             <i class="bi bi-house"></i>
@@ -30,11 +20,18 @@
                         </a>
                     </li>
 
-                        <!-- Option 1 dropdown -->
+                    <li class="nav-item navbar-option" style="height: 100%">
+                        <a class="nav-link" aria-current="true" href="{{route('users.index')}}">
+                            <i class="bi bi-person-circle"></i>
+                            Usuarios
+                        </a>
+                    </li>
+
+                        <!-- Option registros -->
                     <li class="nav-item dropdown navbar-option">
                         <a class="nav-link" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                             <i class="bi bi-list-check"></i>
-                            Opciones
+                            Registros
                         </a>
                         <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                             <li><a class="dropdown-item" href="#">option 1</a></li>
@@ -44,33 +41,20 @@
                         </ul>
                     </li>
 
-                    <!-- Option 2 dropdown -->
+                    <!-- Option herramientas -->
                     <li class="nav-item dropdown navbar-option">
                         <a class="nav-link" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                             <i class="bi bi-list-check"></i>
-                            Opciones
+                            Herramientas
                         </a>
                         <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="#">option 1</a></li>
-                            <li><a class="dropdown-item" href="#">option 2</a></li>
-                            <li><a class="dropdown-item" href="#">option 3</a></li>
-                            <li><a class="dropdown-item" href="#">option 4</a></li>
+                            <li><a class="dropdown-item" href="#">Cambiar contrasenia</a></li>
+                            <li><a class="dropdown-item" href="#">Roles</a></li>
+                            <li><a class="dropdown-item" href="#">Definir Conceptos</a></li>
                         </ul>
                     </li>
 
                     <!-- Option 3 dropdown -->
-                    <li class="nav-item dropdown navbar-option">
-                        <a class="nav-link" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            <i class="bi bi-list-check"></i>
-                            Opciones
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="#">option 1</a></li>
-                            <li><a class="dropdown-item" href="#">option 2</a></li>
-                            <li><a class="dropdown-item" href="#">option 3</a></li>
-                            <li><a class="dropdown-item" href="#">option 4</a></li>
-                        </ul>
-                    </li>
 
                     <li class="nav-item me-2 navbar-option">
                         <a class="nav-link" href="{{ route('logout') }}"
@@ -83,7 +67,6 @@
                             @csrf
                         </form>
                     </li>
-                @endauth
                
             </ul>
 
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,11 +1,32 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="mt-4 row justify-content-center">
+<section class="py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 mx-auto">
+
+                <!-- CUSTOM BLOCKQUOTE -->
+                <blockquote class="blockquote blockquote-custom bg-white p-5 shadow rounded">
+                    <div class="blockquote-custom-icon bg-primary text-white px-2 shadow-sm"> {{$user->name}}</div>
+                    
+                    <footer class="py-4 mt-4 border-top border-bottom">
+                        <p class="mb-0 mt-2 font-italic fs-6">{{$user->name}}</p>
+                        <p class="mb-0 mt-2 font-italic fs-6">{{$user->email}}</p>
+                    </footer>
+                    
+                   <a href="{{route('users.index')}}" class="btn btn-primary my-2">Aceptar</a>
+                </blockquote><!-- END -->
+
+            </div>
+        </div>
+    </div>
+</section>
+    <!--<div class="mt-4 row justify-content-center">
         <div class="col-md-12">
             <div class="card">
                 <div class="card-header">
-                    <h4>Usuario: {{$user->name}}</h4>
+                    <h4>{{$user->name}}</h4>
                 </div>
 
                 <div class="card-body">
@@ -18,5 +39,5 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div>-->
 @endsection
