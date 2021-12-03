bd42b565e71a29a750e544db0473a48f4f4cce9e
DerianCordobaPerez <derianunanleon@gmail.com>
2021-12-02 20:49:50 -0600

    Change file app layouts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,6 @@
 
 <body>
     <div id="app">
-
         <!-- Navbar -->
         @auth
             @include('components.navbar')
@@ -36,9 +35,7 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
-    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/0496ae07d8.js" crossorigin="anonymous"></script>
     @yield('javascript')
 </body>
