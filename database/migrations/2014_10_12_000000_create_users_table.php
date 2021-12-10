4739ea3e827e75ab47bac24cee786e4007465f4b
DerianCordobaPerez <derianunanleon@gmail.com>
2021-12-09 23:35:49 -0600

    Add payment model

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,9 +19,9 @@ class CreateUsersTable extends Migration
             $table->string('lastnames');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->enum('sex', ['male', 'feminine']);
+            $table->string('sex');
             $table->string('identification');
-            $table->enum('maritalStatus', ['single', 'married', 'divorced']);
+            $table->string('maritalStatus');
             $table->string('phone');
             $table->string('nationality');
             $table->string('municipality');
---
+++ database/migrations/2021_12_10_051904_create_payments_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePaymentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->string('concept');
+            $table->string('amount');
+            $table->dateTime('date_made_payment');
+            $table->string('payment_registration_date');
+            $table->string('amount_in_letters');
+            $table->string('observation');
+            $table->string('payment_received');
+            $table->string('account_is_payment');
+            $table->string('identification');
+            $table->string('exchange_rate');
+            $table->string('currency');
+            $table->string('receipt_number');
+            $table->string('pay_time');
+            $table->string('cashier');
+            $table->string('cashier_identification');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+    }
+}
