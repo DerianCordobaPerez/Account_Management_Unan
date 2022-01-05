2239371df07224c5dd8421474fbaba104d4a994e
DerianCordobaPerez <derianunanleon@gmail.com>
2022-01-05 00:26:37 -0600

    Reformat code for viewHelper and modification of the condition roles management

--- app/Helpers/RedirectHelper.php
+++ app/Helpers/RedirectHelper.php
@@ -25,7 +25,7 @@ class RedirectHelper
     public function redirect(array|string $roles, Closure $closure): mixed
     {
         // If the user not contains the required roles, redirect to the home page
-        if (!auth()->user()->authorize($roles) || !auth()->user()->isAdmin())
+        if (!auth()->user()->authorize($roles) && !auth()->user()->isAdmin())
             return redirect()->route('home')->with('error', 'No estas autorizado para acceder a esta ruta');
 
         // If the user contains the required roles, execute the closure
